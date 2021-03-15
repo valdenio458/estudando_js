f420ad1c1f0499900ad3c39b3bac4ea93e3cd542
Valdênio Martinho Pereira <valdeniomartinho@gmail.com>
2021-03-15 10:27:42 -0300

    mia um

---
+++ pratica2.js
@@ -0,0 +1,5 @@
+function modificaCor(mudaCor,l,a){
+    document.getElementById("cor").style.backgroundColor = mudaCor;
+    document.getElementById("cor").style.width = l;
+    document.getElementById("cor").style.height = a;
+}
